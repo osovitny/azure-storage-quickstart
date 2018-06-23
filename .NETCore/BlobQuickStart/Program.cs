6e3e09c332578a33691af4cd92caf2a6bc502ba5
Vadim Osovitny <vadim@osovitny.com>
2018-06-22 20:25:45 -0500

    Files headers added

---
+++ .NETCore/BlobQuickStart/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+<file>
+  Description:
+	  Azure Storage Blob Quickstart: Demonstrate how to upload, list, download, and delete blobs
+
+	Authors:
+		Vadim Osovitny
+    Anatoly Osovitny
+
+	Created:
+		22 Jun 2018
+
+	Copyright (c) 2018 Osovitny Inc. All rights reserved.
+</file>
+*/
+
+using System;
+
+namespace Osovitny.Azure.Storage.Blob.QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ .NETCore/QueueQuickStart/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+<file>
+  Description:
+	  Azure Storage Queue Quickstart: Demonstrate some of the Queue Storage operations
+
+	Authors:
+		Vadim Osovitny
+    Anatoly Osovitny
+
+	Created:
+		22 Jun 2018
+
+	Copyright (c) 2018 Osovitny Inc. All rights reserved.
+</file>
+*/
+
+using System;
+
+namespace Osovitny.Azure.Storage.Queue.QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
